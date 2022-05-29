026969a6b6135228130add7f2aca357b72d14902
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-29 17:21:05 +0500

    Added DB context

--- BookApi_With_Patch/Entities/BookDbContext.cs
+++ BookApi_With_Patch/Entities/BookDbContext.cs
@@ -1,1 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BookApi_With_Patch.Entities;
+
+public class BookDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+}
