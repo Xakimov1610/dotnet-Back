fefe9236b25963485b709adb441c68d3af313349
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-29 17:27:48 +0500

    Add Registration

--- BookApi_With_Patch/Program.cs
+++ BookApi_With_Patch/Program.cs
@@ -1,8 +1,18 @@
+using BookApi_With_Patch.Entities;
+using BookApi_With_Patch.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BookDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+},ServiceLifetime.Singleton);
+
+builder.Services.AddScoped<IBookService, BookService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
